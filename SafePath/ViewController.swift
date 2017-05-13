f74e5c4b8e0799e226a980029b38dc5767822a39
Vineeth Puli <vpuli98@gmail.com>
2017-05-13 15:29:17 -0400

    Initial Commit

---
+++ SafePath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SafePath
+//
+//  Created by Vineeth Puli on 5/13/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
