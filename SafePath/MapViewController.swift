19dee9a18126abef8a56153882cccc543091ce52
Vineeth Puli <vpuli98@gmail.com>
2017-05-13 17:57:08 -0400

    added arcgis map

--- SafePath/MapViewController.swift
+++ SafePath/MapViewController.swift
@@ -11,16 +11,19 @@ import ArcGIS
 
 
 class MapViewController: UIViewController {
-
+    @IBOutlet var mapView: AGSMapView!
+    var damageTable:AGSServiceFeatureTable!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.mapView.map = AGSMap(basemapType: .imageryWithLabelsVector, latitude: 40.7128, longitude: -74.0059, levelOfDetail: 10)
+  
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
 
@@ -33,5 +36,37 @@ class MapViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func mapView(mapView: AGSMapView!, didTapAtPoint screen: CGPoint, mapPoint mappoint: AGSPoint!) {
+        
+        //Add a feature at this point
+//        addFeature(mappoint)
+    }
+/*
+     
+    func addFeature(point: AGSPoint!) {
+        
+        //Create attributes for the feature
+        var attributes = Dictionary<String,NSObject>()
+        attributes["typdamage"] = "Minor" as NSObject
+        attributes["primcause"] = "Earthquake" as NSObject
+        
+        //Create a new feature from the attributes and the point
+        let feature = self.damageTable.createFeature(attributes: attributes, geometry: point)
+        
+        //Add the new feature
+        self.damageTable.add(feature) { [weak self] (error: Error?) -> Void in
+            if let error = error {
+                print("Error while adding feature :: \(error.localizedDescription)")
+            }
+            else {
+                //applied edits on success
+                
+            }
+            //enable interaction with map view
+            self?.mapView.isUserInteractionEnabled = true
+        }
+    }
+*/
+    
 }
